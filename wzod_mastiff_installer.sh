e613782dc58e718bb960fc02737cb671cd44a5b8
Zod <wzod@users.noreply.github.com>
2015-05-06 01:10:02 -0400

    Update wzod_mastiff_installer.sh
    Changelog:
    
    - resolved issues with location of mastiff.conf

--- wzod_mastiff_installer.sh
+++ wzod_mastiff_installer.sh
@@ -176,13 +176,13 @@ install() {
     mkdir /usr/local/etc/yara
     mv -f mastiff-0.6.0 .. ; cd ../mastiff-0.6.0 && chmod +x mas.py
     ln -f -s  "${PWD}"/mas.py /usr/local/bin/mas.py
+    sed -i "/^plugin_dir/ s|\.\/plugins|"$INSTALL_DIR"\/\/mastiff-0.6.0\/plugins|" "$INSTALL_DIR"/mastiff-0.6.0/mastiff.conf
+    sed -i "/^trid\ \=\ / s|\.\/trid|\/usr\/local\/bin|" "$INSTALL_DIR"/mastiff-0.6.0/mastiff.conf
+    sed -i "/^trid_db\ \=\ / s|\.\/trid|\/usr\/local\/etc|" "$INSTALL_DIR"/mastiff-0.6.0/mastiff.conf
+    sed -i "/^yara_sigs\ \=\ / s|\/usr\/local\/|&"etc"\/|" "$INSTALL_DIR"/mastiff-0.6.0/mastiff.conf
+    sed -i "/^disitool\ \=\ / s|\/usr\/local\/bin|"$SETUP_DIR"|" "$INSTALL_DIR"/mastiff-0.6.0/mastiff.conf    
+    sed -i "/^olecmd\=/ s|\/usr\/local\/src|"$SETUP_DIR"|" "$INSTALL_DIR"/mastiff-0.6.0/mastiff.conf
     cp "$INSTALL_DIR"/mastiff-0.6.0/mastiff.conf ~/.mastiff.conf
-    sed -i "/^plugin_dir/ s|\.\/plugins|"$INSTALL_DIR"\/\/mastiff-0.6.0\/plugins|" ~/.mastiff.conf
-    sed -i "/^trid\ \=\ / s|\.\/trid|\/usr\/local\/bin|" ~/.mastiff.conf
-    sed -i "/^trid_db\ \=\ / s|\.\/trid|\/usr\/local\/etc|" ~/.mastiff.conf
-    sed -i "/^yara_sigs\ \=\ / s|\/usr\/local\/|&"etc"\/|" ~/.mastiff.conf
-    sed -i "/^disitool\ \=\ / s|\/usr\/local\/bin|"$SETUP_DIR"|" ~/.mastiff.conf    
-    sed -i "/^olecmd\=/ s|\/usr\/local\/src|"$SETUP_DIR"|" ~/.mastiff.conf
     kill_tail
 } &>>"${LOGFILE}"
 
